225ad39bc0154e65b5fd1e029db5f5c1682b2daf
henry <cksgod16@gmail.com>
2023-05-07 13:10:51 +0900

    pure-function.5 : Imperative to functional

--- src/main/java/grokkingfp/ch02_1.java
+++ src/main/java/grokkingfp/ch02_1.java
@@ -4,25 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ch02_1 {
-    class TipCalculator {
+    static class TipCalculator {
         private List<String> names = new ArrayList<>();
-        private int tipPercentage = 0;
 
-        public void addPerson(String name) {
-            names.add(name);
-            if (names.size() > 5) {
-                tipPercentage = 20;
-            } else if (names.size() > 0) {
-                tipPercentage = 10;
-            }
+        public List<String> addPerson(List<String> names, String name) {
+            List<String> updated = new ArrayList<>(names);
+            updated.add(name);
+            return updated;
         }
 
         public List<String> getNames() {
             return names;
         }
 
-        public int getTipPercentage() {
-            return tipPercentage;
+        public static int getTipPercentage(List<String> names) {
+            if (names.size() > 5) {
+                return 20;
+            } else if (names.size() > 0) {
+                return 10;
+            }
+            return 0;
         }
     }
 }
