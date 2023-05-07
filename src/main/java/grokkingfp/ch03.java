037acb535c62955b2703f51ea99a4ebd93ef5c95
henry <cksgod16@gmail.com>
2023-05-07 16:49:36 +0900

    immutable-values.0 : Base replan method

---
+++ src/main/java/grokkingfp/ch03.java
@@ -0,0 +1,23 @@
+package grokkingfp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ch03 {
+    public static void main(String[] args) {
+        List<String> planA = new ArrayList<>();
+        planA.add("Paris");
+        planA.add("Berlin");
+        planA.add("Krakov");
+        System.out.println("Plan A: " + planA);
+
+        List<String> planB = replan(planA, "Vienna", "Krakov");
+        System.out.println("Plan B" + planB);
+    }
+
+    private static List<String> replan(List<String> plan, String newCity, String beforeCity) {
+        int newCityIndex = plan.indexOf(beforeCity);
+        plan.add(newCityIndex, newCity);
+        return plan;
+    }
+}
