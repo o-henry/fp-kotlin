18cf103f1dd5409c062900df96fdd4c227a3585e
henry <cksgod16@gmail.com>
2023-05-08 10:14:51 +0900

    immutable-values.3 : Mutability to Immutability

--- src/main/java/grokkingfp/ch03_1.java
+++ src/main/java/grokkingfp/ch03_1.java
@@ -15,10 +15,21 @@ public class ch03_1 {
         return sum;
     }
 
-
     static double avgTime(List<Double> lapTimes) {
         double time = totalTime(lapTimes);
-        int laps = lapTimes.size();
+        List<Double> withoutWarmUp = new ArrayList<>(lapTimes);
+        withoutWarmUp.remove(0); // remove warm-up lap
+        int laps = withoutWarmUp.size();
         return time / laps;
+    }
+
+    public static void main(String[] args) {
+        ArrayList lapTimes = new ArrayList<>();
+        lapTimes.add(31.0); // warm-up lap
+        lapTimes.add(20.9);
+        lapTimes.add(21.1);
+        lapTimes.add(21.3);
+        System.out.printf("Total: %.1fs\n", totalTime(lapTimes));
+        System.out.printf("Avg: %.1fs", avgTime(lapTimes));
     }
 }
