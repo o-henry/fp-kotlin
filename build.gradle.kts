3cd0d9facf28795971d9288cd6747a30eeb1c9ee
henry <cksgod16@gmail.com>
2023-05-09 18:45:22 +0900

    I/O.1 : Refactoring to separate the logic and console effect

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("org.http4k:http4k-server-jetty")
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-client-okhttp")
+    implementation("io.arrow-kt:arrow-core:1.2.0-RC")
+    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.0-RC")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/fpwithkotlin/ch13/13.1.kt
+++ src/main/kotlin/fpwithkotlin/ch13/13.1.kt
@@ -1,15 +1,23 @@
 package fpwithkotlin.ch13
 
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
+
 data class Player(val name: String, val score: Int)
 
 fun contest(p1: Player, p2: Player): Unit = // impure function
+    when (val player = winner(p1, p2)) {
+        is Some ->
+            println("${player.value} is thet winner")
+
+        is None ->
+            println("It's a draw!")
+    }
+
+fun winner(p1: Player, p2: Player): Option<Player> =
     when {
-        p1.score > p2.score -> // pure logic
-            println("${p1.name} is the winner!") // I/O
-
-        p1.score < p2.score ->
-            println("${p2.name} is the winner!") // I/O
-
-        else ->
-            println("It's a draw!") // I/O
+        p1.score > p2.score -> Some(p1)
+        p1.score < p2.score -> Some(p2)
+        else -> None
     }
