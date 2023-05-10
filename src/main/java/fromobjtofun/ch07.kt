772f8dc1dd73b79bd932d258608b8f1d599edf4a
henry <cksgod16@gmail.com>
2023-05-10 15:59:46 +0900

    defining-category-in-code.1 : Add functor

--- src/main/java/fromobjtofun/ch07.kt
+++ src/main/java/fromobjtofun/ch07.kt
@@ -1,2 +1,39 @@
 package fromobjtofun
 
+import kotlin.random.Random
+
+/**
+ * category compose to dot and arrow
+ * In code, dots representing our types
+ * arrows representing pure function
+ */
+
+fun <T> identity(a: T): T = a
+
+
+/**
+ * f::A -> C
+ */
+infix fun <A, B, C> ((A) -> B).andThen(f: (B) -> C): (A) -> C = { a: A -> f(this(a)) }
+
+data class Holder<T>(private val value: T) {
+    fun <U> transform(f: (T) -> U): Holder<U> = Holder(f(value))
+
+    companion object {
+        fun <T, U> lift(f: (T) -> U): (Holder<T>) -> Holder<U> = { c: Holder<T> ->
+            c.transform(f)
+        }
+    }
+}
+
+
+fun main() {
+    val a: Holder<String> = Holder("this is a string")
+    val b1: Holder<Int> = a.transform(String::length)
+
+    val strLenLifted = Holder.lift(String::length)
+    val b2: Holder<Int> = strLenLifted(a)
+
+    val a1 = a.transform(::identity)
+    println(a == a1) // true
+}
